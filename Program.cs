fafebe995c8701adce062ec3e7d1f0dabeec46eb
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-04 10:51:48 +0400

    ValidatingSameRegestrationNumber

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AplicationExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students =
+                {
+                new("Ahmed","1234",23),
+                new("salim","1122",33),
+                new("qais","1122",32)
+                };
+
+            Console.WriteLine(Student.ValidatingStudens(students));
+        }
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicationExceptions
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string registratonNumber { get; set; }
+        public int age { get; set; }
+
+        public Student(string name, string registrationNumber, int age) 
+        {
+            this.Name = name;
+            this.registratonNumber = registrationNumber;
+            this.age = age;
+        }
+
+        public static Boolean ValidatingStudens(Student[] students)
+        {
+            for(int i=0; i<students.Length; i++)
+            {
+                for(int j=i+1; j<students.Length; j++)
+                {
+                    if (students[i].registratonNumber == students[j].registratonNumber) 
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
